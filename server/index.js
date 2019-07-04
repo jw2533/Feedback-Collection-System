70e30b1dc071e1e7c7e59bbb8ac0802b3ce62189
Chris <jw2533@cornell.edu>
2019-07-04 00:15:47 -0400

    some change in index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+//import
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
@@ -10,7 +11,7 @@ require('./services/passport');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
-
+//app use express
 const app = express();
 
 app.use(bodyParser.json());
@@ -29,12 +30,13 @@ require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  // like our main.js file, or main.css file!
+  // like main.js file, or main.css file
   app.use(express.static('client/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
+  //a router handler
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
